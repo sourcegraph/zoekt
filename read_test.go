9891f29b871a18f0a54bd76ae1bfea6e34f2796c
Kevin Zheng <kevinzhenng@gmail.com>
2019-08-21 05:34:17 -0700

    add test for backwards compatibility for v15

--- read_test.go
+++ read_test.go
@@ -16,8 +16,13 @@ package zoekt
 
 import (
 	"bytes"
+	"context"
+	"fmt"
+	"os"
 	"reflect"
 	"testing"
+
+	"github.com/google/zoekt/query"
 )
 
 func TestReadWrite(t *testing.T) {
@@ -96,5 +101,188 @@ func TestReadWriteNames(t *testing.T) {
 	}
 	if got := data.fileNameNgrams[stringToNGram("bCd")]; !reflect.DeepEqual(got, []uint32{1}) {
 		t.Errorf("got trigram bcd at bits %v, want sz 2", data.fileNameNgrams)
+	}
+}
+
+func loadShard(fn string) (Searcher, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	iFile, err := NewIndexFile(f)
+	if err != nil {
+		return nil, err
+	}
+	s, err := NewSearcher(iFile)
+	if err != nil {
+		iFile.Close()
+		return nil, fmt.Errorf("NewSearcher(%s): %v", fn, err)
+	}
+
+	return s, nil
+}
+
+func TestReadSearch(t *testing.T) {
+	tcs := []struct {
+		file           string
+		formatVersion  int
+		featureVersion int
+		fileMatches    [][]FileMatch
+	}{
+		{
+			"ctags_zoekt_v16.00000.zoekt",
+			16, 8,
+			[][]FileMatch{{{
+				FileName: "cmd/zoekt/main.go",
+				Language: "go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("func main() {"),
+					LineStart:     1472,
+					LineEnd:       1485,
+					LineNumber:    63,
+					LineFragments: []LineFragmentMatch{{0, 1472, 9, nil}},
+				}},
+			}}, {{
+				FileName: "cmd/zoekt/main.go",
+				Language: "go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("package main"),
+					LineStart:     609,
+					LineEnd:       621,
+					LineNumber:    15,
+					LineFragments: []LineFragmentMatch{{0, 609, 7, nil}},
+				}},
+			}}, {{
+				FileName: "cmd/zoekt/main.go",
+				Language: "go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("func loadShard(fn string) (zoekt.Searcher, error) {"),
+					LineStart:     1135,
+					LineEnd:       1186,
+					LineNumber:    44,
+					LineFragments: []LineFragmentMatch{{9, 1144, 5, &Symbol{"loadShard", "func", "main", "package"}}},
+				}},
+			}}, {{
+				FileName: "cmd/zoekt/main.go",
+				Language: "go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("func loadShard(fn string) (zoekt.Searcher, error) {"),
+					LineStart:     1135,
+					LineEnd:       1186,
+					LineNumber:    44,
+					LineFragments: []LineFragmentMatch{{9, 1144, 5, &Symbol{"loadShard", "func", "main", "package"}}},
+				}},
+			}}},
+		},
+		{
+			"zoekt_v15.00000.zoekt",
+			15, 8,
+			[][]FileMatch{{{
+				FileName: "cmd/zoekt/main.go",
+				Language: "",
+				LineMatches: []LineMatch{{
+					Line:          []byte("func main() {"),
+					LineStart:     1472,
+					LineEnd:       1485,
+					LineNumber:    63,
+					LineFragments: []LineFragmentMatch{{0, 1472, 9, nil}},
+				}},
+			}}, {{
+				FileName: "cmd/zoekt/main.go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("package main"),
+					LineStart:     609,
+					LineEnd:       621,
+					LineNumber:    15,
+					LineFragments: []LineFragmentMatch{{0, 609, 7, nil}},
+				}},
+			}}, {}, {}},
+		},
+		{
+			"zoekt_v16.00000.zoekt",
+			16, 8,
+			[][]FileMatch{{{
+				FileName: "cmd/zoekt/main.go",
+				Language: "",
+				LineMatches: []LineMatch{{
+					Line:          []byte("func main() {"),
+					LineStart:     1472,
+					LineEnd:       1485,
+					LineNumber:    63,
+					LineFragments: []LineFragmentMatch{{0, 1472, 9, nil}},
+				}},
+			}}, {{
+				FileName: "cmd/zoekt/main.go",
+				LineMatches: []LineMatch{{
+					Line:          []byte("package main"),
+					LineStart:     609,
+					LineEnd:       621,
+					LineNumber:    15,
+					LineFragments: []LineFragmentMatch{{0, 609, 7, nil}},
+				}},
+			}}, {}, {}},
+		},
+	}
+
+	qs := []query.Q{
+		query.NewAnd(&query.Substring{Pattern: "func main", Content: true}, &query.Substring{Pattern: "zoekt/main.go", FileName: true}),
+		query.NewAnd(&query.Regexp{Regexp: mustParseRE("^package"), Content: true}, &query.Substring{Pattern: "zoekt/main.go", FileName: true}),
+		query.NewAnd(&query.Symbol{&query.Substring{Pattern: "shard"}}, &query.Substring{Pattern: "zoekt/main.go", FileName: true}),
+		query.NewAnd(&query.Symbol{&query.Regexp{Regexp: mustParseRE("shard$")}}, &query.Substring{Pattern: "zoekt/main.go", FileName: true}),
+	}
+
+	for _, tc := range tcs {
+		shard, err := loadShard("test-index/" + tc.file)
+		if err != nil {
+			t.Fatalf("failed loading shard %s %v", tc.file, err)
+		}
+
+		index, ok := shard.(*indexData)
+		if !ok {
+			t.Fatalf("expected *indexData for %s", tc.file)
+		}
+
+		if index.metaData.IndexFormatVersion != tc.formatVersion {
+			t.Errorf("got %d index format version, want %d for %s", index.metaData.IndexFormatVersion, tc.formatVersion, tc.file)
+		}
+
+		if index.metaData.IndexFeatureVersion != tc.featureVersion {
+			t.Errorf("got %d index feature version, want %d for %s", index.metaData.IndexFeatureVersion, tc.featureVersion, tc.file)
+		}
+
+		for i, q := range qs {
+			res, err := shard.Search(context.Background(), q, &SearchOptions{})
+			if err != nil {
+				t.Fatalf("failed search %s on %s: %v", q, tc.file, err)
+			}
+
+			if len(res.Files) != len(tc.fileMatches[i]) {
+				t.Fatalf("got %d file matches for %s on %s, want %d", len(res.Files), q, tc.file, len(tc.fileMatches[i]))
+			}
+
+			if len(tc.fileMatches[i]) == 0 {
+				continue
+			}
+
+			want := tc.fileMatches[i][0]
+			got := res.Files[0]
+
+			if got.FileName != want.FileName {
+				t.Errorf("got %s file name for %s on %s, want %s", got.FileName, q, tc.file, want.FileName)
+			}
+
+			if got.Language != want.Language {
+				t.Errorf("got %s language for %s on %s, want %s", got.Language, q, tc.file, want.Language)
+			}
+
+			for i, _ := range got.LineMatches {
+				got.LineMatches[i].Score = 0
+			}
+
+			if !reflect.DeepEqual(got.LineMatches, want.LineMatches) {
+				t.Errorf("line matches for %s on %s\ngot:\n%v\nwant:\n%v", q, tc.file, got.LineMatches, want.LineMatches)
+			}
+		}
 	}
 }
