a567791ccde4a802a8ad927c67a228f1fd16e470
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-07-20 16:07:48 +0200

    build: move MergeMeta to indexserver
    Only used by this command.
    
    Change-Id: Ice4e62761e553665a8d2bc2c34963229dd9578a6

--- build/builder.go
+++ build/builder.go
@@ -18,7 +18,6 @@ package build
 
 import (
 	"crypto/sha1"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -706,91 +705,3 @@ func (b *Builder) writeShard(fn string, ib *zoekt.IndexBuilder) (*finishedShard,
 
 // umask holds the Umask of the current process
 var umask os.FileMode
-
-// MergeMeta updates the .meta files for the shards on disk for o.
-//
-// This process is best effort. If anything fails we return on the first
-// failure. This means you might have an inconsistent state on disk if an
-// error is returned. It is recommended to fallback to re-indexing in that
-// case.
-func MergeMeta(o *Options) error {
-	// TODO should this logic live in the zoekt pkg rather than the build pkg?
-	// Argument for build is its the only place we deal with writing to multiple
-	// shards. Argument for zoekt is it's the only place that understands meta
-	// implementation.
-	var todo map[string]string
-	for i := 0; ; i++ {
-		fn := o.ShardName(i)
-
-		repo, _, err := zoekt.ReadMetadataPath(fn)
-		if os.IsNotExist(err) {
-			break
-		} else if err != nil {
-			return err
-		}
-
-		if updated, err := repo.MergeMutable(&o.RepositoryDescription); err != nil {
-			return err
-		} else if !updated {
-			// This shouldn't happen, but ignore it if it does. We may be working on
-			// an interrupted shard. This helps us converge to something correct.
-			continue
-		}
-
-		dst := fn + ".meta"
-		tmp, err := jsonMarshalTmpFile(repo, dst)
-		if err != nil {
-			return err
-		}
-
-		todo[tmp] = dst
-
-		// if we fail to rename, this defer will attempt to remove the tmp file.
-		defer os.Remove(tmp)
-	}
-
-	// best effort once we get here. Rename everything. Return error of last
-	// failure.
-	var renameErr error
-	for tmp, dst := range todo {
-		if err := os.Rename(tmp, dst); err != nil {
-			renameErr = err
-		}
-	}
-
-	return renameErr
-}
-
-// jsonMarshalFileTmp marshals v to the temporary file p + ".*.tmp" and
-// returns the file name.
-//
-// Note: .tmp is the same suffix used by Builder. indexserver knows to clean
-// them up.
-func jsonMarshalTmpFile(v interface{}, p string) (_ string, err error) {
-	b, err := json.Marshal(v)
-	if err != nil {
-		return "", err
-	}
-
-	f, err := ioutil.TempFile(filepath.Dir(p), filepath.Base(p)+".*.tmp")
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		f.Close()
-		if err != nil {
-			_ = os.Remove(f.Name())
-		}
-	}()
-
-	if runtime.GOOS != "windows" {
-		if err := f.Chmod(0o666 &^ umask); err != nil {
-			return "", err
-		}
-	}
-	if _, err := f.Write(b); err != nil {
-		return "", err
-	}
-
-	return f.Name(), f.Close()
-}
