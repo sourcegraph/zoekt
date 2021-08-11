a2c843bdb1bffcfaf674034ddfd35403f90a70ac
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-08-11 18:36:09 +0200

    add shell.nix
    nix-shell provides a way to have isolated development environments.
    Zoekt's development requirements are pretty vanilla: go and git.
    However, I sometimes have issues with getting universal-ctags installed
    across different environments I run.
    
    I've seen some other go projects adopt the use of nix-shell, in
    particular a few of the hashicorp projects. I've ended up using this for
    a while now whenever developing in zoekt and want to share for others.
    
    Change-Id: Icfcc3c7461565fed19aea607b04e03e67b42ce05

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # pkgs.universal-ctags installs the binary as "ctags", not "universal-ctags"
+  # like zoekt expects.
+  ctagsWrapper = pkgs.writeScriptBin "universal-ctags" ''
+#!${pkgs.stdenv.shell}
+exec ${pkgs.universal-ctags}/bin/ctags "$@"
+'';
+
+in pkgs.mkShell {
+  name = "zoekt";
+
+  nativeBuildInputs = [
+    pkgs.go
+
+    # zoekt-git-index
+    pkgs.git
+
+    # Used to index symbols
+    ctagsWrapper
+    pkgs.universal-ctags
+  ];
+}
