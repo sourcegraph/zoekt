11b01f2ed35394f97fb2853af248c45fc859e22a
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-08-18 14:59:06 +0200

    test searching over compound shard

--- cmd/zoekt-merge-index/main_test.go
+++ cmd/zoekt-merge-index/main_test.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"context"
 	"path/filepath"
 	"sort"
 	"testing"
+
+	"github.com/google/zoekt"
+	"github.com/google/zoekt/query"
+	"github.com/google/zoekt/shards"
 )
 
 func TestMerge(t *testing.T) {
-	shards, err := filepath.Glob("../../testdata/shards/*_v16.*.zoekt")
+	dir := t.TempDir()
+
+	v16Shards, err := filepath.Glob("../../testdata/shards/*_v16.*.zoekt")
 	if err != nil {
 		t.Fatal(err)
 	}
-	sort.Strings(shards)
-	t.Log(shards)
+	sort.Strings(v16Shards)
+	t.Log(v16Shards)
 
-	err = merge(t.TempDir(), shards)
+	err = merge(dir, v16Shards)
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	ss, err := shards.NewDirectorySearcher(dir)
+	if err != nil {
+		t.Fatalf("NewDirectorySearcher(%s): %v", dir, err)
+	}
+	defer ss.Close()
+
+	q, err := query.Parse("hello")
+	if err != nil {
+		t.Fatalf("Parse(hello): %v", err)
+	}
+
+	var sOpts zoekt.SearchOptions
+	ctx := context.Background()
+	result, err := ss.Search(ctx, q, &sOpts)
+	if err != nil {
+		t.Fatalf("Search(%v): %v", q, err)
+	}
+
+	// we are merging the same shard twice, so we expect the same file twice.
+	if len(result.Files) != 2 {
+		t.Errorf("got %v, want 2 files.", result.Files)
 	}
 }
