861f13282e6f24fd076aa426dbde9728ba65f746
Tomás Senart <tomas@sourcegraph.com>
2021-09-28 12:42:52 +0100

    RepoSet benchmarks

--- query/marshal_test.go
+++ query/marshal_test.go
@@ -137,16 +137,172 @@ func TestRepoBranchesIDs_Marshal(t *testing.T) {
 	}
 }
 
+func BenchmarkRepoSet_Encode(b *testing.B) {
+	repoBranches := genRepoSet(5_500_000)
+
+	// do one write to amortize away the cost of gob registration
+	w := &countWriter{}
+	enc := gob.NewEncoder(w)
+	if err := enc.Encode(repoBranches); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	b.ReportMetric(float64(w.n), "bytes")
+
+	for n := 0; n < b.N; n++ {
+		if err := enc.Encode(repoBranches); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkRepoSet_Decode(b *testing.B) {
+	repoBranches := genRepoSet(5_500_000)
+
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(repoBranches); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		// We need to include gob.NewDecoder cost to avoid measuring encoding.
+		var repoBranches RepoSet
+		if err := gob.NewDecoder(bytes.NewReader(buf.Bytes())).Decode(&repoBranches); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func TestRepoSet_Marshal(t *testing.T) {
+	want := genRepoSet(1000)
+
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(want); err != nil {
+		t.Fatal(err)
+	}
+
+	var got RepoSet
+	if err := gob.NewDecoder(bytes.NewReader(buf.Bytes())).Decode(&got); err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(want, &got); diff != "" {
+		t.Fatalf("mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func BenchmarkRepoSetIDs_Encode(b *testing.B) {
+	repoBranches := genRepoSetIDs(5_500_000)
+
+	// do one write to amortize away the cost of gob registration
+	w := &countWriter{}
+	enc := gob.NewEncoder(w)
+	if err := enc.Encode(repoBranches); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	b.ReportMetric(float64(w.n), "bytes")
+
+	for n := 0; n < b.N; n++ {
+		if err := enc.Encode(repoBranches); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkRepoSetIDs_Decode(b *testing.B) {
+	repoBranches := genRepoSetIDs(5_500_000)
+
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(repoBranches); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		// We need to include gob.NewDecoder cost to avoid measuring encoding.
+		var repoBranches RepoSet
+		if err := gob.NewDecoder(bytes.NewReader(buf.Bytes())).Decode(&repoBranches); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func TestRepoSetIDs_Marshal(t *testing.T) {
+	want := genRepoSetIDs(1000)
+
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(want); err != nil {
+		t.Fatal(err)
+	}
+
+	var got RepoSet
+	if err := gob.NewDecoder(bytes.NewReader(buf.Bytes())).Decode(&got); err != nil {
+		t.Fatal(err)
+	}
+
+	tr := cmp.Transformer("", func(b *roaring.Bitmap) []uint32 { return b.ToArray() })
+	if diff := cmp.Diff(want, &got, tr); diff != "" {
+		t.Fatalf("mismatch IDs (-want +got):\n%s", diff)
+	}
+}
+
 // Generating 5.5M repos slows down the benchmark setup time, so we cache things.
-var (
-	repoBranchesCache    = map[int]map[string][]string{}
-	repoBranchesIDsCache = map[int]map[string]*roaring.Bitmap{}
-)
+var genCache = map[string]interface{}{}
+
+func genRepoSet(n int) *RepoSet {
+	key := fmt.Sprintf("RepoSet:%d", n)
+	val, ok := genCache[key]
+	if ok {
+		return val.(*RepoSet)
+	}
+
+	rb := genRepoBranches(n)
+	set := make(map[string]bool, len(rb.Set))
+
+	for repo := range rb.Set {
+		set[repo] = true
+	}
+
+	rs := &RepoSet{Set: set}
+	genCache[key] = rs
+	return rs
+}
+
+func genRepoSetIDs(n int) *RepoSet {
+	key := fmt.Sprintf("RepoSetIDs:%d", n)
+	val, ok := genCache[key]
+	if ok {
+		return val.(*RepoSet)
+	}
+
+	set := genRepoSet(n).Set
+	rs := &RepoSet{IDs: roaring.New()}
+
+	for repo := range set {
+		rs.IDs.Add(hash(repo))
+	}
+
+	genCache[key] = rs
+	return rs
+}
 
 func genRepoBranches(n int) *RepoBranches {
-	set, ok := repoBranchesCache[n]
+	key := fmt.Sprintf("RepoBranches:%d", n)
+	val, ok := genCache[key]
 	if ok {
-		return &RepoBranches{Set: set}
+		return val.(*RepoBranches)
 	}
 
 	genName := func(n int) string {
@@ -175,36 +331,38 @@ func genRepoBranches(n int) *RepoBranches {
 		repoIndex++
 	}
 
-	repoBranchesCache[n] = repoBranches.Set
-
+	genCache[key] = repoBranches
 	return repoBranches
 }
 
 func genRepoBranchesIDs(n int) *RepoBranches {
-	branchIDs, ok := repoBranchesIDsCache[n]
+	key := fmt.Sprintf("RepoBranchesIDs:%d", n)
+	val, ok := genCache[key]
 	if ok {
-		return &RepoBranches{IDs: branchIDs}
+		return val.(*RepoBranches)
 	}
 
 	set := genRepoBranches(n).Set
-	branchIDs = map[string]*roaring.Bitmap{}
+	rb := &RepoBranches{IDs: map[string]*roaring.Bitmap{}}
 
 	for repo, branches := range set {
 		for _, branch := range branches {
-			ids, ok := branchIDs[branch]
+			ids, ok := rb.IDs[branch]
 			if !ok {
 				ids = roaring.New()
-				branchIDs[branch] = ids
+				rb.IDs[branch] = ids
 			}
 			ids.Add(hash(repo))
 		}
 	}
 
-	for _, ids := range branchIDs {
+	for _, ids := range rb.IDs {
 		ids.RunOptimize()
 	}
 
-	return &RepoBranches{IDs: branchIDs}
+	genCache[key] = rb
+
+	return rb
 }
 
 func hash(name string) uint32 {
