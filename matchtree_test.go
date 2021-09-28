27a3a14c47032cf43a34eaa0def6f0ba64c93b84
Tomás Senart <tomas@sourcegraph.com>
2021-09-28 10:06:39 +0100

    reduce diff of matchtree_test.go

--- matchtree_test.go
+++ matchtree_test.go
@@ -229,6 +229,29 @@ func TestSymbolMatchRegexAll(t *testing.T) {
 
 func TestRepoSet(t *testing.T) {
 	d := &indexData{
+		repoMetaData:    []Repository{{Name: "r0"}, {Name: "r1"}, {Name: "r2"}, {Name: "r3"}},
+		fileBranchMasks: []uint64{1, 1, 1, 1, 1, 1},
+		repos:           []uint16{0, 0, 1, 2, 3, 3},
+	}
+	mt, err := d.newMatchTree(&query.RepoSet{Set: map[string]bool{"r1": true, "r3": true, "r99": true}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []uint32{2, 4, 5}
+	for i := 0; i < len(want); i++ {
+		nextDoc := mt.nextDoc()
+		if nextDoc != want[i] {
+			t.Fatalf("want %d, got %d", want[i], nextDoc)
+		}
+		mt.prepare(nextDoc)
+	}
+	if mt.nextDoc() != maxUInt32 {
+		t.Fatalf("expected %d document, but got at least 1 more", len(want))
+	}
+}
+
+func TestRepoSet_IDs(t *testing.T) {
+	d := &indexData{
 		repoMetaData: []Repository{
 			{ID: hash("r0"), Name: "r0"},
 			{ID: hash("r1"), Name: "r1"},
@@ -239,48 +262,22 @@ func TestRepoSet(t *testing.T) {
 		repos:           []uint16{0, 0, 1, 2, 3, 3},
 	}
 
-	for _, tc := range []struct {
-		name string
-		rs   *query.RepoSet
-	}{
-		{
-			name: "Set",
-			rs: &query.RepoSet{
-				Set: map[string]bool{"r1": true, "r3": true, "r99": true},
-			},
-		},
-		{
-			name: "IDs",
-			rs: &query.RepoSet{
-				IDs: roaring.BitmapOf(hash("r1"), hash("r3"), hash("r99")),
-			},
-		},
-		{
-			name: "both",
-			rs: &query.RepoSet{
-				Set: map[string]bool{"r1": true, "r3": true, "r99": true},
-				IDs: roaring.BitmapOf(hash("r1"), hash("r3"), hash("r99")),
-			},
-		},
-	} {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			mt, err := d.newMatchTree(tc.rs)
-			if err != nil {
-				t.Fatal(err)
-			}
-			want := []uint32{2, 4, 5}
-			for i := 0; i < len(want); i++ {
-				nextDoc := mt.nextDoc()
-				if nextDoc != want[i] {
-					t.Fatalf("want %d, got %d", want[i], nextDoc)
-				}
-				mt.prepare(nextDoc)
-			}
-			if mt.nextDoc() != maxUInt32 {
-				t.Fatalf("expected %d document, but got at least 1 more", len(want))
-			}
-		})
+	mt, err := d.newMatchTree(&query.RepoSet{
+		IDs: roaring.BitmapOf(hash("r1"), hash("r3"), hash("r99")),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []uint32{2, 4, 5}
+	for i := 0; i < len(want); i++ {
+		nextDoc := mt.nextDoc()
+		if nextDoc != want[i] {
+			t.Fatalf("want %d, got %d", want[i], nextDoc)
+		}
+		mt.prepare(nextDoc)
+	}
+	if mt.nextDoc() != maxUInt32 {
+		t.Fatalf("expected %d document, but got at least 1 more", len(want))
 	}
 }
 
@@ -309,6 +306,30 @@ func TestRepo(t *testing.T) {
 
 func TestRepoBranches(t *testing.T) {
 	d := &indexData{
+		repoMetaData:    []Repository{{Name: "foo"}, {Name: "bar"}},
+		fileBranchMasks: []uint64{1, 1, 1, 2, 1, 2, 1},
+		repos:           []uint16{0, 0, 1, 1, 1, 1, 1},
+		branchIDs:       []map[string]uint{{"HEAD": 1}, {"HEAD": 1, "b1": 2}},
+	}
+	mt, err := d.newMatchTree(&query.RepoBranches{Set: map[string][]string{"bar": {"b1", "b2"}}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []uint32{3, 5}
+	for i := 0; i < len(want); i++ {
+		nextDoc := mt.nextDoc()
+		if nextDoc != want[i] {
+			t.Fatalf("want %d, got %d", want[i], nextDoc)
+		}
+		mt.prepare(nextDoc)
+	}
+	if mt.nextDoc() != maxUInt32 {
+		t.Fatalf("expect %d documents, but got at least 1 more", len(want))
+	}
+}
+
+func TestRepoBranches_IDs(t *testing.T) {
+	d := &indexData{
 		repoMetaData: []Repository{
 			{ID: hash("foo"), Name: "foo"},
 			{ID: hash("bar"), Name: "bar"},
@@ -318,53 +339,26 @@ func TestRepoBranches(t *testing.T) {
 		branchIDs:       []map[string]uint{{"HEAD": 1}, {"HEAD": 1, "b1": 2}},
 	}
 
-	for _, tc := range []struct {
-		name string
-		rb   *query.RepoBranches
-	}{
-		{
-			name: "Set",
-			rb: &query.RepoBranches{
-				Set: map[string][]string{"bar": {"b1", "b2"}},
-			},
+	mt, err := d.newMatchTree(&query.RepoBranches{
+		IDs: map[string]*roaring.Bitmap{
+			"b1": roaring.BitmapOf(hash("bar")),
+			"b2": roaring.BitmapOf(hash("bar")),
 		},
-		{
-			name: "IDs",
-			rb: &query.RepoBranches{
-				IDs: map[string]*roaring.Bitmap{
-					"b1": roaring.BitmapOf(hash("bar")),
-					"b2": roaring.BitmapOf(hash("bar")),
-				},
-			},
-		},
-		{
-			name: "both",
-			rb: &query.RepoBranches{
-				Set: map[string][]string{"bar": {"b1", "b2"}},
-				IDs: map[string]*roaring.Bitmap{
-					"b1": roaring.BitmapOf(hash("bar")),
-					"b2": roaring.BitmapOf(hash("bar")),
-				},
-			},
-		},
-	} {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			mt, err := d.newMatchTree(tc.rb)
-			if err != nil {
-				t.Fatal(err)
-			}
-			want := []uint32{3, 5}
-			for i := 0; i < len(want); i++ {
-				nextDoc := mt.nextDoc()
-				if nextDoc != want[i] {
-					t.Fatalf("want %d, got %d", want[i], nextDoc)
-				}
-				mt.prepare(nextDoc)
-			}
-			if mt.nextDoc() != maxUInt32 {
-				t.Fatalf("expect %d documents, but got at least 1 more", len(want))
-			}
-		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := []uint32{3, 5}
+	for i := 0; i < len(want); i++ {
+		nextDoc := mt.nextDoc()
+		if nextDoc != want[i] {
+			t.Fatalf("want %d, got %d", want[i], nextDoc)
+		}
+		mt.prepare(nextDoc)
+	}
+
+	if mt.nextDoc() != maxUInt32 {
+		t.Fatalf("expect %d documents, but got at least 1 more", len(want))
 	}
 }
