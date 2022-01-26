1d26e5d7676b3165e184050b6607bf164ef1f5b2
Doudou <648530+kisabaka@users.noreply.github.com>
2022-01-27 00:09:12 +0100

    Fix cbuild failure.

--- web/server.go
+++ web/server.go
@@ -270,11 +270,12 @@ func (s *Server) serveSearchErr(r *http.Request) (*ApiSearchResult, error) {
 	}
 
 	numCtxLines := 0
-	if qvals.Get("format") == "json" && qvals.Has("ctx") {
-		ctxLinesStr := qvals.Get("ctx")
-		numCtxLines, err = strconv.Atoi(ctxLinesStr)
-		if err != nil || numCtxLines < 0 || numCtxLines > 10 {
-			return nil, fmt.Errorf("Number of context lines must be between 0 and 10")
+	if qvals.Get("format") == "json" {
+		if ctxLinesStr := qvals.Get("ctx"); ctxLinesStr != "" {
+			numCtxLines, err = strconv.Atoi(ctxLinesStr)
+			if err != nil || numCtxLines < 0 || numCtxLines > 10 {
+				return nil, fmt.Errorf("Number of context lines must be between 0 and 10")
+			}
 		}
 	}
 	sOpts.NumContextLines = numCtxLines
