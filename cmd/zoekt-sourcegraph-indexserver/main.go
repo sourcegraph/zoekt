339a90517c9a56d9e5ec2cd01d01d14fc898656e
Stefan Hengl <stefan@sourcegraph.com>
2022-01-26 09:05:39 +0100

    merging: run vacuum every 24h instead of every hour
    We are probably too aggresive with running vacuum every hour.
    Additionally, we will soon support soft-deletion of repos within
    compound shards IE unsetting tombstones. THis a longer time between
    vacuum makes sense.

--- cmd/zoekt-sourcegraph-indexserver/main.go
+++ cmd/zoekt-sourcegraph-indexserver/main.go
@@ -723,7 +723,7 @@ func main() {
 
 	root := flag.String("sourcegraph_url", os.Getenv("SRC_FRONTEND_INTERNAL"), "http://sourcegraph-frontend-internal or http://localhost:3090. If a path to a directory, we fake the Sourcegraph API and index all repos rooted under path.")
 	interval := flag.Duration("interval", time.Minute, "sync with sourcegraph this often")
-	vacuumInterval := flag.Duration("vacuum_interval", time.Hour, "run vacuum this often")
+	vacuumInterval := flag.Duration("vacuum_interval", 24*time.Hour, "run vacuum this often")
 	mergeInterval := flag.Duration("merge_interval", time.Hour, "run merge this often")
 	targetSize := flag.Int64("merge_target_size", getEnvWithDefaultInt64("SRC_TARGET_SIZE", 2000), "the target size of compound shards in MiB")
 	maxSize := flag.Int64("merge_max_size", getEnvWithDefaultInt64("SRC_MAX_SIZE", 1800), "the maximum size in MiB a shard can have to be considered for merging")
