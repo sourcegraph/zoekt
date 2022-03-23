b656b832a8ef0cca4d55146617caa6d53888de4f
Stefan Hengl <stefan@sourcegraph.com>
2022-03-23 15:30:36 +0100

    chmod +x

--- download-ctags-installer.sh
+++ download-ctags-installer.sh
@@ -3,3 +3,4 @@ set -x
 # Commit from 2022-03-01. Please always pick a commit from the main branch.
 export SOURCEGRAPH_COMMIT=20497508d57afd4bbd35597629779255d772a7f8
 wget https://raw.githubusercontent.com/sourcegraph/sourcegraph/$SOURCEGRAPH_COMMIT/cmd/symbols/ctags-install-alpine.sh -P $1
+chmod +x "$1/ctags-install-alpine.sh"
