81309fd18ab6d80d03201437dfc5a4bef15b1b7c
Stefan Hengl <stefan@sourcegraph.com>
2022-03-23 15:25:40 +0100

    bin/sh

--- download-ctags-installer.sh
+++ download-ctags-installer.sh
@@ -1,4 +1,4 @@
-#!/user/bin/env sh
+#!/bin/sh
 set -x
 # Commit from 2022-03-01. Please always pick a commit from the main branch.
 export SOURCEGRAPH_COMMIT=20497508d57afd4bbd35597629779255d772a7f8
