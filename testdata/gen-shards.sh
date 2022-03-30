b733b1c4e734c462d92ea353c96e891bb5b3e064
Geoffrey Gilmore <me@ggilmore.net>
2022-03-30 10:43:56 -0700

    fix shellcheck errors in gen-shards.sh

--- testdata/gen-shards.sh
+++ testdata/gen-shards.sh
@@ -11,9 +11,9 @@ mv compound*zoekt repo17_v17.00000.zoekt
 
 rm -rf repo17 repo17_v16.00000.zoekt zoekt-builder-shard-log.tsv
 
-mv *.zoekt shards/
+mv ./*.zoekt shards/
 
 # generate repo2.v16.0000.zoekt
 go run ../cmd/zoekt-index repo2
 rm zoekt-builder-shard-log.tsv
-mv *.zoekt shards/
+mv ./*.zoekt shards/
