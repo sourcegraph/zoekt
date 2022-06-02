4ce144522c94d822f17e6c5e50cfdd2eea5fd8c7
Camden Cheek <camden@ccheek.com>
2022-06-02 15:44:19 -0600

    add test for atOffset

--- contentprovider_test.go
+++ contentprovider_test.go
@@ -56,3 +56,73 @@ four`)
 		}
 	}
 }
+
+func TestAtOffset(t *testing.T) {
+	cases := []struct {
+		data       []byte
+		offset     uint32
+		lineNumber int
+		lineStart  int
+		lineEnd    int
+	}{{
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     0,
+		lineNumber: 1, lineStart: 0, lineEnd: 6,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     6,
+		lineNumber: 1, lineStart: 0, lineEnd: 6,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     2,
+		lineNumber: 1, lineStart: 0, lineEnd: 6,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     2,
+		lineNumber: 1, lineStart: 0, lineEnd: 6,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     7,
+		lineNumber: 2, lineStart: 7, lineEnd: 14,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     11,
+		lineNumber: 2, lineStart: 7, lineEnd: 14,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11.\n"),
+		offset:     15,
+		lineNumber: 3, lineStart: 15, lineEnd: 15,
+	}, {
+		data:       []byte("0.2.4.\n7.9.11."),
+		offset:     7,
+		lineNumber: 2, lineStart: 7, lineEnd: 14,
+	}, {
+		data:       []byte("\n\n"),
+		offset:     0,
+		lineNumber: 1, lineStart: 0, lineEnd: 0,
+	}, {
+		data:       []byte("\n\n"),
+		offset:     1,
+		lineNumber: 2, lineStart: 1, lineEnd: 1,
+	}, {
+		data:       []byte("\n\n"),
+		offset:     3,
+		lineNumber: 3, lineStart: 2, lineEnd: 2,
+	}}
+
+	for _, tt := range cases {
+		t.Run("", func(t *testing.T) {
+			nls := getNewlines(tt.data)
+			gotLineNumber, gotLineStart, gotLineEnd := nls.atOffset(tt.offset)
+			if gotLineNumber != tt.lineNumber {
+				t.Fatalf("expected line number %d, got %d", tt.lineNumber, gotLineNumber)
+			}
+			if gotLineStart != tt.lineStart {
+				t.Fatalf("expected line start %d, got %d", tt.lineStart, gotLineStart)
+			}
+			if gotLineEnd != tt.lineEnd {
+				t.Fatalf("expected line end %d, got %d", tt.lineEnd, gotLineEnd)
+			}
+		})
+	}
+}
