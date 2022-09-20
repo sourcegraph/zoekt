6500eb19f526198674cfc14dd05294c496255dbe
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-09-20 09:30:27 +0200

    add memprofile

--- cmd/git-sg/main.go
+++ cmd/git-sg/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"strings"
 
@@ -303,21 +304,36 @@ func openGitRepo() (*git.Repository, error) {
 }
 
 func main() {
-	cpuProfile := flag.String("cpu_profile", "", "write cpu profile to `file`")
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile := flag.String("memprofile", "", "write memory profile to `file`")
 	flag.Parse()
 
-	if *cpuProfile != "" {
-		f, err := os.Create(*cpuProfile)
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("could not create CPU profile: ", err)
 		}
-		defer f.Close()
-		pprof.StartCPUProfile(f)
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
 		defer pprof.StopCPUProfile()
 	}
 
 	err := do(os.Stdout)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
 	}
 }
