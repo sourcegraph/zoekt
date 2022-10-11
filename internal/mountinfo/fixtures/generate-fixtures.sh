92cf39de65f8c8aac02477ad297a288cc3985649
Geoffrey Gilmore <me@ggilmore.net>
2022-10-11 16:43:16 -0700

    ci: add bash and sed to test runner

--- internal/mountinfo/fixtures/generate-fixtures.sh
+++ internal/mountinfo/fixtures/generate-fixtures.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#
+# Regenerates the contents of the fixtures/sys directory
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -euxo pipefail
 
 rm -rf sys || true
 
-./ttar.sh -C "$(pwd)" -x -f ./sys.ttar
+./ttar.sh -v -C "$(pwd)" -x -f ./sys.ttar
