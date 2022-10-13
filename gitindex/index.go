f9346c32fe66ca28db8f2f6fc3953bb2b8cbcbc3
Stefan Hengl <stefan@sourcegraph.com>
2022-10-13 13:28:34 +0200

    fix triple slash

--- gitindex/index.go
+++ gitindex/index.go
@@ -114,7 +114,7 @@ func setTemplates(repo *zoekt.Repository, u *url.URL, typ string) error {
 	repo.URL = u.String()
 	switch typ {
 	case "gitiles":
-		// / eg. https://gerrit.googlesource.com/gitiles/+/master/tools/run_dev.sh#20
+		// eg. https://gerrit.googlesource.com/gitiles/+/master/tools/run_dev.sh#20
 		repo.CommitURLTemplate = u.String() + "/+/{{.Version}}"
 		repo.FileURLTemplate = u.String() + "/+/{{.Version}}/{{.Path}}"
 		repo.LineFragmentTemplate = "#{{.LineNumber}}"
