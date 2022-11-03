9eb454fb3c74bb2cbf0865f820138ed7221f1b0f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-11-03 11:49:12 +0200

    disable shellcheck for pipefail
    We want pipefail behavior and in practice this seems to work in busybox
    sh.

--- install-ctags-alpine.sh
+++ install-ctags-alpine.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eux
-set -o pipefail
+# shellcheck disable=SC3040
+set -o pipefail || true
 # Commit from 2022-04-05. Please always pick a commit from the main branch.
 export SOURCEGRAPH_COMMIT=4dd4ce3d91da5cac2ac6169d3005714247178f57
 wget -O - https://raw.githubusercontent.com/sourcegraph/sourcegraph/$SOURCEGRAPH_COMMIT/cmd/symbols/ctags-install-alpine.sh | sh
