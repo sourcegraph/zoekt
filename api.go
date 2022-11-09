2e71aad6ac9ace4bed7febc928ca466ea3c3afd9
Stefan Hengl <stefan@sourcegraph.com>
2022-11-09 15:22:33 +0100

    ranking: parametrize contribution of document ranks (#479)
    We tune the contribution of document ranks to the final ranking with a damping factor. The zero value of the damping factor leads to an equal contribution of scores and ranks, while a damping factor of 1 effectively eliminates the influence of document ranks.
    
    The default is 0 IE equal contribution.

--- api.go
+++ api.go
@@ -803,6 +803,11 @@ type SearchOptions struct {
 	// sorting matches.
 	UseDocumentRanks bool
 
+	// RanksDampingFactor determines the contribution of documents ranks to the
+	// final ranking based on RRF. A value in (0,1] reduces the contribution,
+	// while a value in (-inf,0) increases it.
+	RanksDampingFactor float64
+
 	// Trace turns on opentracing for this request if true and if the Jaeger address was provided as
 	// a command-line flag
 	Trace bool
