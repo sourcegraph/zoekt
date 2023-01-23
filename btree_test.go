abbcc8478753d9329618ca9630ea7e23c5d7a2ac
Stefan Hengl <stefan@sourcegraph.com>
2023-01-23 16:57:16 +0100

    fix tests

--- btree_test.go
+++ btree_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestBTree_unsorted(t *testing.T) {
-	bt := newBtree(2, 2)
+	bt := newBtree(btreeOpts{bucketSize: 2, v: 2})
 	insertMany(t, bt, []ngram{6, 2, 4, 3, 9, 8, 7, 5, 1})
 	// inner nodes only
 	//
@@ -22,7 +22,7 @@ func TestBTree_unsorted(t *testing.T) {
 }
 
 func TestBTree_sorted(t *testing.T) {
-	bt := newBtree(2, 2)
+	bt := newBtree(btreeOpts{bucketSize: 2, v: 2})
 	insertMany(t, bt, []ngram{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 	// inner nodes only
 	//
@@ -37,7 +37,7 @@ func TestBTree_sorted(t *testing.T) {
 }
 
 func TestSerialization(t *testing.T) {
-	bt := newBtree(2, 2)
+	bt := newBtree(btreeOpts{bucketSize: 2, v: 2})
 	insertMany(t, bt, []ngram{6, 2, 4, 3, 9, 8, 7, 5, 1})
 
 	var buf bytes.Buffer
@@ -57,7 +57,7 @@ func TestSerialization(t *testing.T) {
 }
 
 func TestFindBucket(t *testing.T) {
-	bt := newBtree(2, 2)
+	bt := newBtree(btreeOpts{bucketSize: 2, v: 2})
 	insertMany(t, bt, []ngram{6, 2, 4, 3, 9, 8, 7, 5, 1})
 
 	buckets := 0
