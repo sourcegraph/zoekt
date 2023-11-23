387bbae94a71f7b21e4e5f80437224bfd1efd908
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2023-11-22 16:11:51 -0800

    Remove timeout from parser wrapper

--- ctags/json.go
+++ ctags/json.go
@@ -15,9 +15,6 @@
 package ctags
 
 import (
-	"fmt"
-	"time"
-
 	goctags "github.com/sourcegraph/go-ctags"
 )
 
@@ -26,81 +23,28 @@ const debug = false
 type Parser = goctags.Parser
 type Entry = goctags.Entry
 
-type parseReq struct {
-	Name    string
-	Content []byte
-}
-
-type parseResp struct {
-	Entries []*Entry
-	Err     error
-}
-
 type lockedParser struct {
 	opts goctags.Options
 	p    Parser
-	send chan<- parseReq
-	recv <-chan parseResp
 }
 
-// parseTimeout is how long we wait for a response for parsing a single file
-// in ctags. 1 minute is a very conservative timeout which we should only hit
-// if ctags hangs.
-const parseTimeout = time.Minute
-
-// Parse wraps go-ctags Parse. It lazily starts the process and adds a timeout
-// around parse requests. The timeout is important since we occasionally come
-// across documents which hang universal-ctags.
+// Parse wraps go-ctags Parse and lazily starts the process.
 func (lp *lockedParser) Parse(name string, content []byte) ([]*Entry, error) {
 	if lp.p == nil {
 		p, err := goctags.New(lp.opts)
 		if err != nil {
 			return nil, err
 		}
-		send := make(chan parseReq)
-		// buf of 1 so we avoid blocking sends in the parser if we exit early.
-		recv := make(chan parseResp, 1)
-
-		go func() {
-			defer close(recv)
-			for req := range send {
-				entries, err := p.Parse(req.Name, req.Content)
-				recv <- parseResp{Entries: entries, Err: err}
-			}
-		}()
-
 		lp.p = p
-		lp.send = send
-		lp.recv = recv
 	}
 
-	lp.send <- parseReq{Name: name, Content: content}
-
-	deadline := time.NewTimer(parseTimeout)
-	defer deadline.Stop()
-
-	select {
-	case resp := <-lp.recv:
-		return resp.Entries, resp.Err
-	case <-deadline.C:
-		// Error out since ctags hanging is a sign something bad is happening.
-		lp.close()
-		return nil, fmt.Errorf("ctags timedout after %s parsing %s", parseTimeout, name)
-	}
+	return lp.p.Parse(name, content)
 }
 
 func (lp *lockedParser) Close() {
-	lp.close()
-}
-
-func (lp *lockedParser) close() {
 	if lp.p == nil {
 		return
 	}
-
 	lp.p.Close()
 	lp.p = nil
-	close(lp.send)
-	lp.send = nil
-	lp.recv = nil
 }
