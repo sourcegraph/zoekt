12ce07a298aed45c4ee9fa92f9acfdcb83f0836f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-07-26 18:01:08 +0200

    index: experiment to limit ngram lookups for large snippets (#795)
    This introduces an experiment where we can stop looking up ngrams at a
    certain limit. The insight here is that for large substrings we spend
    more time finding the smallest ngram frequency than the time a normal
    search takes. So instead we can try and find a good balance between
    looking for a good (two) ngrams and actually searching the corpus.
    
    The plan is to set different values for
    SRC_EXPERIMENT_ITERATE_NGRAM_LOOKUP_LIMIT in sourcegraph production and
    see how it affects performance of attribution search service.
    
    Test Plan: ran all tests with the envvar set to 2. I expected tests that
    assert on stats to fail, but everything else to pass. This was the case.
    
      SRC_EXPERIMENT_ITERATE_NGRAM_LOOKUP_LIMIT=2 go test ./...

--- bits.go
+++ bits.go
@@ -124,6 +124,11 @@ func (a runeNgramOff) Compare(b runeNgramOff) int {
 }
 
 func splitNGrams(str []byte) []runeNgramOff {
+	// len(maxNgrams) >= the number of ngrams in str => no limit
+	return splitNGramsLimit(str, len(str))
+}
+
+func splitNGramsLimit(str []byte, maxNgrams int) []runeNgramOff {
 	var runeGram [3]rune
 	var off [3]uint32
 	var runeCount int
@@ -131,7 +136,7 @@ func splitNGrams(str []byte) []runeNgramOff {
 	result := make([]runeNgramOff, 0, len(str))
 	var i uint32
 
-	for len(str) > 0 {
+	for len(str) > 0 && len(result) < maxNgrams {
 		r, sz := utf8.DecodeRune(str)
 		str = str[sz:]
 		runeGram[0] = runeGram[1]
