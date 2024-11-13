3e094aedb0ec0a88ff568a2699e5a103ab2e84fe
Stefan Hengl <stefan@sourcegraph.com>
2024-11-13 11:40:16 +0100

    pprof log missing tenants

--- internal/tenant/internal/enforcement/enforcement.go
+++ internal/tenant/internal/enforcement/enforcement.go
@@ -6,3 +6,16 @@ import "go.uber.org/atomic"
 // instead of in the tenant package to avoid a circular dependency. See
 // tenanttest.MockEnforce.
 var EnforcementMode atomic.String
+
+// ShouldLogNoTenant returns true if the tenant enforcement mode is logging or strict.
+// It is used to log a warning if a request to a low-level store is made without a tenant
+// so we can identify missing tenants. This will go away and only strict will be allowed
+// once we are confident that all contexts carry tenants.
+func ShouldLogNoTenant() bool {
+	switch EnforcementMode.Load() {
+	case "logging", "strict":
+		return true
+	default:
+		return false
+	}
+}
--- internal/tenant/internal/tenanttype/type.go
+++ internal/tenant/internal/tenanttype/type.go
@@ -3,7 +3,12 @@ package tenanttype
 import (
 	"context"
 	"fmt"
+	"runtime/pprof"
 	"strconv"
+
+	"github.com/sourcegraph/zoekt/internal/tenant/internal/enforcement"
+
+	"go.uber.org/atomic"
 )
 
 type Tenant struct {
@@ -24,15 +29,36 @@ func WithTenant(ctx context.Context, tenant *Tenant) context.Context {
 	return context.WithValue(ctx, tenantKey, tenant)
 }
 
+const skipLogging contextKey = iota
+
 var ErrNoTenantInContext = fmt.Errorf("no tenant in context")
 
 func FromContext(ctx context.Context) (*Tenant, error) {
 	tnt, ok := ctx.Value(tenantKey).(*Tenant)
 	if !ok {
+		if pprofMissingTenant != nil {
+			_, ok := ctx.Value(skipLogging).(contextKey)
+			if !ok {
+				// We want to track every stack trace, so need a unique value for the event
+				eventValue := pprofUniqID.Add(1)
+
+				// skip stack for Add and this function (2).
+				pprofMissingTenant.Add(eventValue, 2)
+			}
+		}
+
 		return nil, ErrNoTenantInContext
 	}
 	return tnt, nil
 }
+
+var pprofUniqID atomic.Int64
+var pprofMissingTenant = func() *pprof.Profile {
+	if !enforcement.ShouldLogNoTenant() {
+		return nil
+	}
+	return pprof.NewProfile("missing_tenant")
+}()
 
 func Unmarshal(s string) (*Tenant, error) {
 	id, err := strconv.Atoi(s)
