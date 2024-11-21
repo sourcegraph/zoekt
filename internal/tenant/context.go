c1295f90e53275859ad63389c4f8a7e94b45bb08
Stefan Hengl <stefan@sourcegraph.com>
2024-11-21 12:20:56 +0100

    tenant: add tenant to trace (#862)
    This adds the tenant ID to the trace. I also move the pprof logging from `FromContext` to the higher level `typeRepoSearcher`. The number of events was too high, because we logged missing tenants per document.
    
    I also fixed a bug where pprof logging didn't work at all, because we read the tenant enforcemnt ENV after we set the pprof profile, so the profile was always nil.
    
    Test plan:
    Checked locally that tenants show up in the traces and "missing_tenant" shows up as pprof profile.

--- internal/tenant/context.go
+++ internal/tenant/context.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"runtime/pprof"
+	"sync"
 
 	"go.uber.org/atomic"
 
 	"github.com/sourcegraph/zoekt/internal/tenant/internal/enforcement"
 	"github.com/sourcegraph/zoekt/internal/tenant/internal/tenanttype"
+	"github.com/sourcegraph/zoekt/trace"
 )
 
 var ErrMissingTenant = fmt.Errorf("missing tenant")
@@ -16,23 +18,53 @@ var ErrMissingTenant = fmt.Errorf("missing tenant")
 func FromContext(ctx context.Context) (*tenanttype.Tenant, error) {
 	tnt, ok := tenanttype.GetTenant(ctx)
 	if !ok {
-		if pprofMissingTenant != nil {
-			// We want to track every stack trace, so need a unique value for the event
-			eventValue := pprofUniqID.Add(1)
-
-			// skip stack for Add and this function (2).
-			pprofMissingTenant.Add(eventValue, 2)
-		}
-
 		return nil, ErrMissingTenant
 	}
 	return tnt, nil
 }
 
-var pprofUniqID atomic.Int64
-var pprofMissingTenant = func() *pprof.Profile {
-	if !enforcement.ShouldLogNoTenant() {
-		return nil
+// Log logs the tenant ID to the trace. If tenant logging is enabled, it also
+// logs a stack trace to a pprof profile.
+func Log(ctx context.Context, tr *trace.Trace) {
+	tnt, ok := tenanttype.GetTenant(ctx)
+	if !ok {
+		if profile := pprofMissingTenant(); profile != nil {
+			// We want to track every stack trace, so need a unique value for the event
+			eventValue := pprofUniqID.Add(1)
+
+			// skip stack for Add and this function (2).
+			profile.Add(eventValue, 2)
+		}
+		tr.LazyPrintf("tenant: missing")
+		return
 	}
-	return pprof.NewProfile("missing_tenant")
-}()
+	tr.LazyPrintf("tenant: %d", tnt.ID())
+}
+
+var pprofUniqID atomic.Int64
+var pprofOnce sync.Once
+var pprofProfile *pprof.Profile
+
+// pprofMissingTenant returns the pprof profile for missing tenants,
+// initializing it only once.
+func pprofMissingTenant() *pprof.Profile {
+	pprofOnce.Do(func() {
+		if shouldLogNoTenant() {
+			pprofProfile = pprof.NewProfile("missing_tenant")
+		}
+	})
+	return pprofProfile
+}
+
+// shouldLogNoTenant returns true if the tenant enforcement mode is logging or strict.
+// It is used to log a warning if a request to a low-level store is made without a tenant
+// so we can identify missing tenants. This will go away and only strict will be allowed
+// once we are confident that all contexts carry tenants.
+func shouldLogNoTenant() bool {
+	switch enforcement.EnforcementMode.Load() {
+	case "logging", "strict":
+		return true
+	default:
+		return false
+	}
+}
--- internal/tenant/internal/enforcement/enforcement.go
+++ internal/tenant/internal/enforcement/enforcement.go
@@ -1,21 +1,12 @@
 package enforcement
 
-import "go.uber.org/atomic"
+import (
+	"os"
+
+	"go.uber.org/atomic"
+)
 
 // EnforcementMode is the current tenant enforcement mode. It resides here
 // instead of in the tenant package to avoid a circular dependency. See
 // tenanttest.MockEnforce.
-var EnforcementMode atomic.String
-
-// ShouldLogNoTenant returns true if the tenant enforcement mode is logging or strict.
-// It is used to log a warning if a request to a low-level store is made without a tenant
-// so we can identify missing tenants. This will go away and only strict will be allowed
-// once we are confident that all contexts carry tenants.
-func ShouldLogNoTenant() bool {
-	switch EnforcementMode.Load() {
-	case "logging", "strict":
-		return true
-	default:
-		return false
-	}
-}
+var EnforcementMode = atomic.NewString(os.Getenv("SRC_TENANT_ENFORCEMENT_MODE"))
