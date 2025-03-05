7a31afd0cef0d253bd16fb013778ae63dd0a4bc6
Stefan Hengl <stefan@sourcegraph.com>
2025-03-05 10:45:01 +0100

    format

--- cmd/zoekt-sourcegraph-indexserver/grpc/server/server.go
+++ cmd/zoekt-sourcegraph-indexserver/grpc/server/server.go
@@ -10,18 +10,21 @@ import (
 	"github.com/sourcegraph/zoekt/grpc/defaults"
 )
 
-type Server struct {
-	logger sglog.Logger
-
-	indexserverv1.UnimplementedSourcegraphIndexserverServiceServer
-}
-
 func NewServer(logger sglog.Logger, additionalOpts ...grpc.ServerOption) *grpc.Server {
 	s := defaults.NewServer(logger, additionalOpts...)
 	indexserverv1.RegisterSourcegraphIndexserverServiceServer(s, &Server{logger: logger})
 	return s
 }
 
+// Server implements the SourcegraphIndexserverService gRPC service.
+type Server struct {
+	logger sglog.Logger
+
+	indexserverv1.UnimplementedSourcegraphIndexserverServiceServer
+}
+
+// DeleteAllData deletes all shards in the index and trash belonging to the
+// tenant associated with the request. This is stubbed out for now.
 func (s *Server) DeleteAllData(ctx context.Context, req *indexserverv1.DeleteAllDataRequest) (*indexserverv1.DeleteAllDataResponse, error) {
 	s.logger.Warn("DeleteAllData")
 	return &indexserverv1.DeleteAllDataResponse{}, nil
