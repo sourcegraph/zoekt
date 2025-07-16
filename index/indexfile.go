29870750cad194a3540021e31bcb20ba408af8c0
Matthias Fechner <mfechner@users.noreply.github.com>
2025-07-16 18:24:37 +0200

    Allow building on FreeBSD
    This fixes https://github.com/sourcegraph/zoekt/issues/967.
    
    I reported that also here: https://gitlab.com/gitlab-org/gitlab-zoekt-indexer/-/issues/91

--- index/indexfile.go
+++ index/indexfile.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || darwin
+//go:build linux || darwin || freebsd
 
 package index
 
