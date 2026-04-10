c1e4e777a85486e5a4d92ab696557692c3c1bca7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2026-04-10 12:54:02 +0200

    Update .github/workflows/docker-version.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .github/workflows/docker-version.sh
+++ .github/workflows/docker-version.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # This is the pseudo-version that go.mod uses. We use the same version string
 # so that downstream consumers can line up image versions with module versions.
 version="$(TZ=UTC git --no-pager show \
-	--quiet \
-	--abbrev=12 \
-	--date='format-local:%Y%m%d%H%M%S' \
-	--format='0.0.0-%cd-%h')"
+  --quiet \
+  --abbrev=12 \
+  --date='format-local:%Y%m%d%H%M%S' \
+  --format='0.0.0-%cd-%h')"
 
 printf 'value=%s\n' "$version" >>"$GITHUB_OUTPUT"
