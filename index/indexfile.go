3c8b39b1ef4f8194cb912d7e6581cff9db224aa7
Thomas Klausner <wiz@gatalith.at>
2026-07-17 11:53:32 +0200

    index: fix build on NetBSD (#1091)

--- index/indexfile.go
+++ index/indexfile.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || darwin || freebsd
+//go:build linux || darwin || freebsd || netbsd
 
 package index
 
