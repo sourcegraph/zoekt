feb7e5e52ad906dc23710336b1bd54139b91c332
gemelen <oss@gemelen.net>
2026-08-11 10:40:47 -0300

    Extend build for illumos platform

--- index/indexfile.go
+++ index/indexfile.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || darwin || freebsd || netbsd
+//go:build linux || darwin || freebsd || netbsd || illumos
 
 package index
 
