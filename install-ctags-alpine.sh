a5455cc668c7d232ec59da07ddafd6f9e4004a0c
Ondrej Kokes <ondrej.kokes@aisle.com>
2026-08-13 09:45:41 +0200

    move shas

--- install-ctags-alpine.sh
+++ install-ctags-alpine.sh
@@ -9,17 +9,20 @@
 
 CTAGS_VERSION=${CTAGS_VERSION:-2026.08.11}
 CTAGS_COMMIT=${CTAGS_COMMIT:-8361949f6a2465fb1bbaf26a234278c3c3cbd3ac}
+# GitHub release asset digests for uctags-${CTAGS_VERSION}-linux-*.release.apk
+CTAGS_APK_SHA256_X86_64=bde53a3092fd540e004bf7923322a21604592734aea002086764f02456378c9d
+CTAGS_APK_SHA256_AARCH64=245239f8097a2877ad91d14e742670d25ecd5bac7d2b77fdb0139dfcce291d22
 
 set -eux
 
 case "${TARGETARCH:-$(uname -m)}" in
   amd64 | x86_64)
     apk_name="uctags-${CTAGS_VERSION}-linux-x86_64.release.apk"
-    apk_sha256=bde53a3092fd540e004bf7923322a21604592734aea002086764f02456378c9d
+    apk_sha256=$CTAGS_APK_SHA256_X86_64
     ;;
   arm64 | aarch64)
     apk_name="uctags-${CTAGS_VERSION}-linux-aarch64.release.apk"
-    apk_sha256=245239f8097a2877ad91d14e742670d25ecd5bac7d2b77fdb0139dfcce291d22
+    apk_sha256=$CTAGS_APK_SHA256_AARCH64
     ;;
   *) echo "unsupported architecture: ${TARGETARCH:-$(uname -m)}" >&2; exit 1 ;;
 esac
