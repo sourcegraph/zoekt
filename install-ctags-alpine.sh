5f0c2a62b73a9cc93c1ae3883f75d8fdc6bfc817
Ondrej Kokes <ondrej.kokes@aisle.com>
2026-08-13 09:21:06 +0200

    sha verify

--- install-ctags-alpine.sh
+++ install-ctags-alpine.sh
@@ -2,6 +2,10 @@
 
 # Installs universal-ctags from prebuilt Alpine APKs (amd64/arm64).
 # Zoekt looks up "universal-ctags" on PATH; the APK installs "ctags".
+#
+# sha256 sums below are GitHub release asset digests. When bumping
+# CTAGS_VERSION or CTAGS_COMMIT, copy them from the release page/API:
+# https://github.com/universal-ctags/ctags-nightly-build/releases
 
 CTAGS_VERSION=${CTAGS_VERSION:-2026.08.11}
 CTAGS_COMMIT=${CTAGS_COMMIT:-8361949f6a2465fb1bbaf26a234278c3c3cbd3ac}
@@ -9,17 +13,25 @@ CTAGS_COMMIT=${CTAGS_COMMIT:-8361949f6a2465fb1bbaf26a234278c3c3cbd3ac}
 set -eux
 
 case "${TARGETARCH:-$(uname -m)}" in
-  amd64 | x86_64) ctags_arch=x86_64 ;;
-  arm64 | aarch64) ctags_arch=aarch64 ;;
+  amd64 | x86_64)
+    apk_name="uctags-${CTAGS_VERSION}-linux-x86_64.release.apk"
+    apk_sha256=bde53a3092fd540e004bf7923322a21604592734aea002086764f02456378c9d
+    ;;
+  arm64 | aarch64)
+    apk_name="uctags-${CTAGS_VERSION}-linux-aarch64.release.apk"
+    apk_sha256=245239f8097a2877ad91d14e742670d25ecd5bac7d2b77fdb0139dfcce291d22
+    ;;
   *) echo "unsupported architecture: ${TARGETARCH:-$(uname -m)}" >&2; exit 1 ;;
 esac
 
-apk_name="uctags-${CTAGS_VERSION}-linux-${ctags_arch}.release.apk"
 base_url="https://github.com/universal-ctags/ctags-nightly-build/releases/download/${CTAGS_VERSION}%2B${CTAGS_COMMIT}"
+apk_path="/tmp/${apk_name}"
 
-wget -qO "/tmp/${apk_name}" "${base_url}/${apk_name}"
+wget -qO "$apk_path" "${base_url}/${apk_name}"
 wget -qO /etc/apk/keys/uctags.rsa.pub "${base_url}/${apk_name}.rsa.pub"
-apk add --no-cache "/tmp/${apk_name}"
-rm "/tmp/${apk_name}"
+
+echo "$apk_sha256  $apk_path" | sha256sum -c -
+apk add --no-cache "$apk_path"
+rm "$apk_path"
 
 ln -sf /usr/bin/ctags /usr/bin/universal-ctags
